a7c702ecb3aef88ef836e1a60d09a69525254918
Joachim Jensen <jv@intox.dk>
2015-02-19 22:07:59 -0800

    view for module data in group

---
+++ view/module/group.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package WP Content Aware Engine
+ * @version 1.0
+ * @copyright Joachim Jensen <jv@intox.dk>
+ * @license GPLv3
+ */
+?>
+
+<div class="cas-condition cas-condition-'.$id.'">
+	<h4><?php echo $name; ?></h4>
+	<ul>
+<?php if(in_array($id,$data)) : ?>
+		<li>
+			<label>
+				<input type="checkbox" name="cas_condition[<?php echo $id; ?>][]" value="<?php echo $id; ?>" checked="checked" /><?php printf(__("All %s",WPCACore::DOMAIN),$name); ?>
+			</label>
+		</li>
+<?php
+	endif;
+	echo $checkboxes;
+?>
+		</ul>
+</div>
