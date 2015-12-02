d66ccbec2b2040f85f40d8ad594a1df884e6e499
Joachim Jensen <jv@intox.dk>
2015-12-02 20:20:24 +0100

    option to hide condition meta box is obsolete

--- posttypemanager.php
+++ posttypemanager.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @package WP Content Aware Engine
- * @version 1.0
  * @copyright Joachim Jensen <jv@intox.dk>
  * @license GPLv3
  */
@@ -23,7 +22,6 @@ if(!class_exists("WPCAPostTypeManager")) {
 		 */
 		public function __construct() {
 			parent::__construct();
-			add_action('init',array($this,'deploy'),98);
 		}
 
 		/**
@@ -37,31 +35,6 @@ if(!class_exists("WPCAPostTypeManager")) {
 				$post_type = get_post_type_object($post_type);
 			}
 			parent::add($post_type,$post_type->name);
-		}
-
-		/**
-		 * Deploy post types
-		 * 
-		 * @since   1.0
-		 * @return  void
-		 */
-		public function deploy() {
-			foreach($this->get_all() as $post_type) {
-				add_filter('manage_'.$post_type->name.'_columns',
-					array(&$this,'metabox_preferences'));
-			}
-		}
-
-		/**
-		 * Display module in Screen Settings
-		 *
-		 * @since   1.0
-		 * @param   array    $columns
-		 * @return  array
-		 */
-		public function metabox_preferences($columns) {
-			$columns['_title'] = __("Conditional Content",WPCACore::DOMAIN);
-			return $columns;
 		}
 	}
 }
